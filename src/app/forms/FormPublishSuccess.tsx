c48d4474ae1a723a4c8a13c1a3a1bb93dcc515f4
Piyush yadav <piyushyadav0191@gmail.com>
2024-02-22 11:26:00 +0530

    url changed to production

--- src/app/forms/FormPublishSuccess.tsx
+++ src/app/forms/FormPublishSuccess.tsx
@@ -17,7 +17,7 @@ type Props = {
 }
 
 const FormPublishSuccess = (props: Props) => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://aiformulator.vercel.app';
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(baseUrl + '/forms/' + props.formId)
